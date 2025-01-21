34e1e2f30e7e47f3d01e5094796d0369e6baaca0
Bhrugen Patel <bhrugen1990@gmail.com>
2025-01-21 17:00:25 -0500

    Section 5 - Call API from Web Project

--- AzureB2CWeb/Controllers/HomeController.cs
+++ AzureB2CWeb/Controllers/HomeController.cs
@@ -11,10 +11,11 @@ namespace AzureB2CWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private IHttpClientFactory _httpClientFactory;
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
         {
             _logger = logger;
+            _httpClientFactory = httpClientFactory;
         }
 
         public IActionResult Index()
@@ -52,6 +53,22 @@ namespace AzureB2CWeb.Controllers
             {
                 RedirectUri = "/"
             }, "B2C_1_edit");
+        }
+
+        public async Task<IActionResult> APICall()
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:44323/WeatherForecast");
+
+            var response = await client.SendAsync(request);
+
+            if(response.StatusCode!= System.Net.HttpStatusCode.OK)
+            {
+                //error
+            }
+
+            return View("APICall",await response.Content.ReadAsStringAsync());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- AzureB2CWeb/Program.cs
+++ AzureB2CWeb/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews().AddMicrosoftIdentityUI();
 builder.Services.AddRazorPages();
+builder.Services.AddHttpClient();
 //builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
 //    .AddMicrosoftIdentityWebApp(option => {
 //        builder.Configuration.Bind("AzureB2C", option);
